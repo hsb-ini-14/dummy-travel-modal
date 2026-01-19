e26159dc2f5bb52c0185fff986c2b6ffe8067b14
hsb-ini-14 <bhaduria14ini@gmail.com>
2026-01-19 19:50:12 +0530

    completed dummyTravelModal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,118 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import BG from "./components/BG";
+import plane from "./assets/plane.png";
+import cancel from "./assets/cancel.png";
+import { useEffect, useRef, useState } from "react";
+import { flightOffers, images } from "./data";
 
-export default App
+const getRandomNumber = () => Math.floor(Math.random() * 41) - 20;
+
+const App = () => {
+  const modalWrapperRef = useRef(null);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClickOutside = (event) => {
+    if (modalWrapperRef.current === event.target) {
+      setShowModal(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <>
+      <BG />
+      <div className="centerChild h-screen">
+        {/* Button */}
+        <button
+          onClick={() => setShowModal(true)}
+          className={`relative cursor-pointer group overflow-hidden py-3 perspective-midrange ${showModal ? "overflow-visible -rotate-y-[25deg]" : "overflow-hidden"}`}
+        >
+          <div className="bg-zinc-700 px-7 py-3 border-dotted border-[3px] border-cyan-300 bg-clip-padding rounded-lg hover:-rotate-y-[25deg] duration-500 transition-transform">
+            <span
+              className={`block text-white text-2xl group-hover:opacity-0 group-hover:-translate-y-24 transition-all duration-300 group-hover:delay-75 delay-300 ${showModal && "-translate-y-24"}`}
+            >
+              Book Your Flight
+            </span>
+            <span
+              className={`absolute h-[2px] w-full border border-dashed border-white left-0 top-1/2 -translate-y-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-right group-hover:delay-200 ${showModal && "scale-x-100"}`}
+            />
+            <img
+              src={plane}
+              alt="Plane Icon"
+              className={`absolute -left-28 top-1/2 -translate-y-1/2 group-hover:left-20 group-hover:transition-all group-hover:delay-500 group-hover:duration-500 ${showModal && "transition-all !left-44 !scale-150 !opacity-0 !delay-[0s]"}`}
+            />
+          </div>
+        </button>
+        {/* Modal Wrapper */}
+        <div
+          ref={modalWrapperRef}
+          className={`centerChild absolute w-full h-full bg-fuchsia-200/50 -z-10 opacity-0 invisible transition-opacity ${showModal && "!opacity-100 !visible !z-10 delay-500"}`}
+        >
+          {/* Modal */}
+          <div className="centerChild flex-col gap-y-8 relative w-[730px] h-[600px] bg-white rounded-3xl">
+            {/* Close Button */}
+            <button
+              onClick={() => setShowModal(false)}
+              className="absolute top-4 right-5 cursor-pointer hover:scale-110 transition-transform"
+            >
+              <img src={cancel} alt="Cancelicon" width={25} />
+            </button>
+            {/* Modal Heading */}
+            <h1 className="centerChild gap-x-3 text-3xl font-semibold text-gray-800">
+              Book your trip around the{" "}
+              <div className="relative globeWrapper">
+                <div className="globe"></div>
+                <img
+                  src={plane}
+                  alt="Plane Icon"
+                  className={`absolute right-52 top-1/2 -translate-y-1/2 scale-150 opacity-40 ${showModal && "!-right-2 !scale-100 transition-all delay-500 duration-500 opacity-100"}`}
+                />
+              </div>
+            </h1>
+            {/* Gallery */}
+            <div className="centerChild gap-6">
+              {images.map((image, index) => (
+                <img
+                  src={image}
+                  alt="Image"
+                  key={index}
+                  className="w-32 aspect-square object-cover border-[3px] border-dotted border-cyan-500 rounded-3xl opacity-80 hover:z-10 hover:opacity-100 hover:scale-125 transition-all"
+                  style={{ transform: `rotate(${getRandomNumber()}deg)` }}
+                />
+              ))}
+            </div>
+            {/* Flight offers */}
+            <div className="grid grid-cols-2 gap-3 w-full px-10">
+              {flightOffers.map((offer, index) => (
+                <p className="text-xs font-semibold text-gray-700" key={index}>
+                  {offer}
+                </p>
+              ))}
+            </div>
+            {/* Buttons */}
+            <div className="absolute bottom-4 right-4 space-x-2">
+              <button className="rounded-sm px-2 py-1 text-xs cursor-pointer bg-cyan-500 text-white hover:bg-cyan-600 transition-colors">
+                Book Now
+              </button>
+              <button
+                onClick={() => setShowModal(false)}
+                className="rounded-sm px-2 py-1 text-xs cursor-pointer bg-gray-100 border border-gray-300 text-cyan-600 hover:bg-gray-200 transition-colors"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/data/index.js
@@ -0,0 +1,16 @@
+import img1 from "../assets/images/img-1.jpg";
+import img2 from "../assets/images/img-2.jpg";
+import img3 from "../assets/images/img-3.jpg";
+import img4 from "../assets/images/img-4.jpg";
+import img5 from "../assets/images/img-5.jpg";
+
+export const images = [img1, img2, img3, img4, img5];
+
+export const flightOffers = [
+  "✈️ 20% off on international flights – Limited time!",
+  "💺 Free seat upgrade on business class bookings.",
+  "🛄 Extra baggage allowance for frequent flyers.",
+  "🏨 Flight + Hotel combo deals with 30% savings.",
+  "🎟️ Buy one, get one free on select domestic routes.",
+  "🚀 Earn double loyalty points on all bookings this month.",
+];
